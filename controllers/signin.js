c77be1fb1026138badb3a16e2fc27bfe5ac9aa02
frozenparadox99 <frostbitecryengine300@gmail.com>
2019-08-18 07:08:27 +0530

    added api endpoints

---
+++ controllers/signin.js
@@ -0,0 +1,30 @@
+const handleSignin= (req,res,db,bcrypt)=>{
+    const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json('incorrect form submission');
+  }
+    db.select('email','hash').from('login')
+    .where('email','=',req.body.email)
+    .then(data=>{
+       const isValid= bcrypt.compareSync(req.body.password,data[0].hash);
+       if(isValid){
+          return db.select('*').from('users').where('email','=',req.body.email)
+           .then(
+               user=>{
+                   res.json(user[0])
+               }
+           )
+           .catch(err=>res.status(400).json('Not Working'))
+       }
+       else{
+        res.status(400).json('Wrong')
+       }
+       
+    })
+
+    .catch(err=>res.status(400).json('Wrong Credentials'))
+}
+
+module.exports={
+    handleSignin:handleSignin
+}
---
+++ server.js
@@ -0,0 +1,86 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const bcrypt=require('bcrypt-nodejs');
+const cors=require('cors');
+const knex=require('knex')
+
+const register=require('./controllers/register');
+const signin=require('./controllers/signin');
+const profile=require('./controllers/profile');
+const image=require('./controllers/image')
+
+const PORT=process.env.PORT||4000;
+
+const db= knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'sushantlenka',
+      password : '',
+      database : 'smart-brain'
+    }
+  })
+
+  
+
+const app=express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+const database={
+    users:[
+        {
+            id:"123",
+            name:'John',
+            email:'john@cena.com',
+            password:'1234567',
+            entries:0,
+            joined: new Date()
+
+        },
+        {
+            id:"124",
+            name:'Randy',
+            email:'randy@orton.com',
+            password:'1234567',
+            entries:0,
+            joined: new Date()
+
+        }
+    ],
+    login:[
+        {
+            id:'955',
+            hash:'',
+            email:'john@cena.com'
+        }
+    ]
+}
+
+app.get('/',(req,res)=>{
+    res.json(database.users);
+})
+
+app.get('/profile/:id',(req,res)=>{
+    profile.handleProfileGet(req,res,db)
+})
+
+app.post('/signin',(req,res)=>{
+    signin.handleSignin(req,res,db,bcrypt)
+})
+
+app.post('/register',(req,res)=>{register.handleRegister(req,res,db,bcrypt)})
+
+
+app.put('/image',(req,res)=>{
+    image.handleImage(req,res,db)
+})
+
+app.post('/imageurl',(req,res)=>{
+    image.handleApiCall(req,res)
+})
+
+app.listen(PORT,(req,res)=>{
+    console.log(`Server started at port ${PORT}`);
+});
